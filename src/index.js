bedcfc88fa684aae5a6f80645ff3e48047f826c0
richie.buckingham <richie.buckingham@gmail.com>
2018-09-01 19:03:02 +1000

    create a simple component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create the model for the application
+let model = { clicks: -12 };
+
+function render() {
+  ReactDOM.render(
+    <App
+      clicks={model.clicks}
+      onClick={() => {
+        model.clicks += 1;
+        render();
+      }}
+    />,
+    document.getElementById("root")
+  );
+}
+
+render();
+
 registerServiceWorker();
